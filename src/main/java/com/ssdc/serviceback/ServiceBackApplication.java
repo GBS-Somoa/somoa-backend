28ede25b682e0099fd00a150919fb55997400206
www-jong <weon1009@gmail.com>
2024-03-14 17:53:00 +0900

    Init:Service back 서버 생성

---
+++ src/main/java/com/ssdc/serviceback/ServiceBackApplication.java
@@ -0,0 +1,13 @@
+package com.ssdc.serviceback;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ServiceBackApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ServiceBackApplication.class, args);
+    }
+
+}
