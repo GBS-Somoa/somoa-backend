4233e2c219c12fc0b665dfc1321914457cc5cdc2
www-jong <weon1009@gmail.com>
2024-03-18 17:40:24 +0900

    Chore: blockhound 설정(버그미해결)

---
+++ src/main/java/com/ssdc/serviceback/global/config/BlockHoundCustomConfiguration.java
@@ -0,0 +1,19 @@
+package com.ssdc.serviceback.global.config;
+
+import reactor.blockhound.BlockHound;
+import reactor.blockhound.integration.BlockHoundIntegration;
+
+public class BlockHoundCustomConfiguration implements BlockHoundIntegration {
+
+    @Override
+    public void applyTo(BlockHound.Builder builder){
+        builder.allowBlockingCallsInside("io.netty.util.concurrent.FastThreadLocalRunnable", "run")
+                .allowBlockingCallsInside("java.io.FileInputStream", "readBytes")
+                .allowBlockingCallsInside("java.security.Provider$Service", "newInstance")
+                .allowBlockingCallsInside("sun.security.ssl.SSLContextImpl$DefaultSSLContext", "<init>")
+                .allowBlockingCallsInside(
+                "io.netty.handler.ssl.SslContext",
+                "newClientContextInternal")
+        ;
+    }
+}
