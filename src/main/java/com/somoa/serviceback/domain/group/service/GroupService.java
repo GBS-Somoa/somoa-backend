976f39f2d60e4616af7cc931f130b45ea5a51a76
gusckd96 <gusckd96@naver.com>
2024-03-21 14:56:43 +0900

    Feat: 그룹 생성 서비스 로직 생성
    - GroupRepository, GroupUserRepository 생성
    - GroupRegisterParam DTO로 입력
    - TODO: 유저별 그룹 순서 번호 지정 기능

---
+++ src/main/java/com/somoa/serviceback/domain/group/service/GroupService.java
@@ -0,0 +1,46 @@
+package com.somoa.serviceback.domain.group.service;
+
+import com.somoa.serviceback.domain.group.dto.GroupRegisterParam;
+import com.somoa.serviceback.domain.group.entity.Group;
+import com.somoa.serviceback.domain.group.repository.GroupRepository;
+import com.somoa.serviceback.domain.groupuser.entity.GroupUser;
+import com.somoa.serviceback.domain.groupuser.entity.GroupUserRole;
+import com.somoa.serviceback.domain.groupuser.repository.GroupUserRepository;
+import com.somoa.serviceback.domain.user.entity.User;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+import reactor.core.publisher.Mono;
+
+import java.util.HashMap;
+import java.util.Map;
+
+@Service
+@RequiredArgsConstructor
+public class GroupService {
+
+    private final GroupRepository groupRepository;
+    private final GroupUserRepository groupUserRepository;
+
+    @Transactional
+    public Mono<Map<String, Object>> save(Integer userId, GroupRegisterParam param) {
+        return groupRepository.save(Group.builder()
+                        .name(param.getGroupName())
+                        .build())
+                .flatMap(savedGroup -> {
+                    GroupUser groupUser = GroupUser.builder()
+                            .groupId(savedGroup.getId())
+                            .userId(userId)
+                            .role(GroupUserRole.MANAGER)
+                            .orderedNum(0)  // TODO: orderNum 맨 마지막으로 할당해야 함
+                            .build();
+                    return groupUserRepository.save(groupUser);
+                })
+                .map(groupUser -> {
+                    Map<String, Object> data = new HashMap<>();
+                    data.put("groupId", groupUser.getGroupId());
+                    data.put("groupUserId", groupUser.getId());
+                    return data;
+                });
+    }
+}
