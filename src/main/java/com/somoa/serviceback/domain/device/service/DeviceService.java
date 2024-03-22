eb0f21abce1ffd65eea8783ae73bdc08a8464167
양유경 <gosjorb@gmail.com>
2024-03-23 01:13:19 +0900

    Feat: 제조사 서버에서 기기 정보 받아오는 로직

--- src/main/java/com/somoa/serviceback/domain/device/service/DeviceService.java
+++ src/main/java/com/somoa/serviceback/domain/device/service/DeviceService.java
@@ -1,11 +1,15 @@
 package com.somoa.serviceback.domain.device.service;
 
 import com.somoa.serviceback.domain.device.dto.DeviceRegisterParam;
+import com.somoa.serviceback.domain.device.dto.DeviceResponse;
 import com.somoa.serviceback.domain.device.entity.Device;
 import com.somoa.serviceback.domain.device.repository.DeviceRepository;
+import com.somoa.serviceback.global.handler.ResponseHandler;
 import lombok.RequiredArgsConstructor;
+import org.springframework.core.ParameterizedTypeReference;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+import org.springframework.web.reactive.function.client.WebClient;
 import reactor.core.publisher.Mono;
 
 @Service
@@ -13,6 +17,9 @@ import reactor.core.publisher.Mono;
 public class DeviceService {
 
     private final DeviceRepository deviceRepository;
+    private final String MANUFACTURER_SERVER_URL = "http://localhost:9090";
+    private final String DEVICE_API_PATH = "/api/device";
+    private final String DEVICE_ID_QUERY_PARAM = "device_id";
 
     @Transactional
     public Mono<?> save(DeviceRegisterParam param) {
@@ -22,22 +29,40 @@ public class DeviceService {
         final String model = "모델 이름";
         final String type = "타입";
         final String manufacturer = "제조사";
+
+        // Mono<DeviceResponse> responseMono = getDeviceResponse(param.getCode());
+            Mono<DeviceResponse> responseMono = Mono.just(DeviceResponse.builder()
+                    .model(model)
+                    .type(type)
+                    .manufacturer(manufacturer)
+                    .build());
         // ****************************************
 
-        return deviceRepository.findByCode(param.getCode())
+        return responseMono.flatMap(response -> deviceRepository.findByCode(param.getCode()))
                 .flatMap(existingDevice -> Mono.error(new IllegalArgumentException("이미 등록된 기기입니다.")))
-                .switchIfEmpty(Mono.defer(() -> {
+                .switchIfEmpty(responseMono.flatMap(response -> {
                             Device device = Device.builder()
                                     .code(param.getCode())
                                     .groupId(param.getGroupId())
                                     .nickname(param.getNickname())
-                                    .model(model)
-                                    .type(type)
-                                    .manufacturer(manufacturer)
+                                    .model(response.getModel())
+                                    .type(response.getType())
+                                    .manufacturer(response.getManufacturer())
                                     .build();
                             return deviceRepository.save(device)
                                     .map(savedDevice -> Mono.empty());
                         }
                 ));
+    }
+
+    public Mono<DeviceResponse> getDeviceResponse(String deviceId) {
+        WebClient webClient = WebClient.create(MANUFACTURER_SERVER_URL);
+
+        return webClient.get()
+                .uri(uriBuilder -> uriBuilder.path(DEVICE_API_PATH)
+                        .queryParam(DEVICE_ID_QUERY_PARAM, deviceId)
+                        .build())
+                .retrieve()
+                .bodyToMono(DeviceResponse.class);
     }
 }
