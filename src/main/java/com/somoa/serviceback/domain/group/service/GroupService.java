acf746804b6b85e96d03d30f90312f6c2b4cb4f9
gusckd96 <gusckd96@naver.com>
2024-03-22 23:32:05 +0900

    Feat: 그룹 멤버 추가 로직 구현

--- src/main/java/com/somoa/serviceback/domain/group/service/GroupService.java
+++ src/main/java/com/somoa/serviceback/domain/group/service/GroupService.java
@@ -9,6 +9,7 @@ import org.springframework.transaction.annotation.Transactional;
 import com.somoa.serviceback.domain.group.dto.GroupDetailResponse;
 import com.somoa.serviceback.domain.group.dto.GroupRegisterParam;
 import com.somoa.serviceback.domain.group.dto.GroupResponse;
+import com.somoa.serviceback.domain.group.dto.GroupUserRegisterParam;
 import com.somoa.serviceback.domain.group.entity.Group;
 import com.somoa.serviceback.domain.group.repository.GroupRepository;
 import com.somoa.serviceback.domain.groupuser.entity.GroupUser;
@@ -64,6 +65,36 @@ public class GroupService {
                             .map(groupMembers -> GroupDetailResponse.of(group, groupMembers)));
                 } else {
                     return Mono.error(new IllegalArgumentException("장소에 대한 권한이 없는 유저입니다."));
+                }
+            });
+    }
+
+    @Transactional
+    public Mono<Object> addMember(Integer groupId, GroupUserRegisterParam param) {
+        final Integer userId = param.getUserId();
+
+        // response data
+        Map<String, Object> data = new HashMap<>();
+        data.put("groupId", groupId);
+        data.put("userId", userId);
+
+        return groupUserRepository.existsGroupUser(groupId, userId)
+            .flatMap(userExists -> {
+                if (userExists) {
+                    return Mono.error(new IllegalArgumentException("이미 그룹에 등록된 유저입니다."));
+                } else {
+                    return Mono.defer(() -> {
+                            GroupUser groupUser = GroupUser.builder()
+                                .groupId(groupId)
+                                .userId(userId)
+                                .role(GroupUserRole.USER_ALL)
+                                .orderedNum(0)  // TODO: orderNum 맨 마지막으로 할당해야 함
+                                .alarm(true)
+                                .build();
+                            return groupUserRepository.save(groupUser)
+                                .then(Mono.just(data));
+                        }
+                    );
                 }
             });
     }
