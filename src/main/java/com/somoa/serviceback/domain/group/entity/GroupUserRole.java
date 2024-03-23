5f4f5e8e29a50e8e57689fd9e6cb4153ef7bbd10
gusckd96 <gusckd96@naver.com>
2024-03-24 00:15:28 +0900

    Feat: 그룹 멤버 권한 수정 메서드 구현

--- src/main/java/com/somoa/serviceback/domain/group/entity/GroupUserRole.java
+++ src/main/java/com/somoa/serviceback/domain/group/entity/GroupUserRole.java
@@ -6,4 +6,16 @@ public class GroupUserRole {
     public static final String USER_ALL = "모든 권한";
     public static final String USER_ONLY_DEVICE_MANAGE = "기기 관리";
     public static final String USER_ONLY_SUPPLY_MANAGE = "소모품 관리";
+
+    private static final String[] roles = {
+        MANAGER, USER_ALL, USER_ONLY_DEVICE_MANAGE, USER_ONLY_SUPPLY_MANAGE
+    };
+
+    public static boolean isValidRole(String role) {
+        for (String _role: roles) {
+            if (role.equals(_role))
+                return true;
+        }
+        return false;
+    }
 }
