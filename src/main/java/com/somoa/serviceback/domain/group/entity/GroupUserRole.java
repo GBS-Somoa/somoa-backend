9b602dfdd9249e5ce602645cd44e7e7a3b7e44c0
gusckd96 <gusckd96@naver.com>
2024-03-24 00:16:50 +0900

    Fix: 그룹 멤버 권한 '관리자'로 설정할 수 있는 문제 해결

--- src/main/java/com/somoa/serviceback/domain/group/entity/GroupUserRole.java
+++ src/main/java/com/somoa/serviceback/domain/group/entity/GroupUserRole.java
@@ -7,13 +7,13 @@ public class GroupUserRole {
     public static final String USER_ONLY_DEVICE_MANAGE = "기기 관리";
     public static final String USER_ONLY_SUPPLY_MANAGE = "소모품 관리";
 
-    private static final String[] roles = {
-        MANAGER, USER_ALL, USER_ONLY_DEVICE_MANAGE, USER_ONLY_SUPPLY_MANAGE
+    private static final String[] userRoles = {
+        USER_ALL, USER_ONLY_DEVICE_MANAGE, USER_ONLY_SUPPLY_MANAGE
     };
 
     public static boolean isValidRole(String role) {
-        for (String _role: roles) {
-            if (role.equals(_role))
+        for (String userRole: userRoles) {
+            if (role.equals(userRole))
                 return true;
         }
         return false;
