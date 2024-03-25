fb8948d0671ec1664dcfeecf26062acf37640729
www-jong <weon1009@gmail.com>
2024-03-25 09:32:50 +0900

    Fix: extends 오타 수정

--- src/main/java/com/somoa/serviceback/domain/supplies/repository/SuppliesRepository.java
+++ src/main/java/com/somoa/serviceback/domain/supplies/repository/SuppliesRepository.java
@@ -1,7 +1,9 @@
 package com.somoa.serviceback.domain.supplies.repository;
 
 import com.somoa.serviceback.domain.supplies.entity.Supplies;
+import org.springframework.data.mongodb.repository.ReactiveMongoRepository;
 import org.springframework.data.repository.reactive.ReactiveCrudRepository;
 
-public interface SuppliesRepository extends ReactiveCrudRepository<Supplies, String> {
+public interface SuppliesRepository extends ReactiveMongoRepository<Supplies, String> {
+
 }
