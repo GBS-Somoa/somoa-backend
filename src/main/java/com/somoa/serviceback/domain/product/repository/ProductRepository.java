dece2a7712f860843bcab5cf6fa888823c47f69f
gusckd96 <gusckd96@naver.com>
2024-03-29 11:13:10 +0900

    Feat: Product Repository 수정

--- src/main/java/com/somoa/serviceback/domain/product/repository/ProductRepository.java
+++ src/main/java/com/somoa/serviceback/domain/product/repository/ProductRepository.java
@@ -2,9 +2,9 @@ package com.somoa.serviceback.domain.product.repository;
 
 import com.somoa.serviceback.domain.product.entity.Product;
 import org.springframework.data.repository.reactive.ReactiveCrudRepository;
-import reactor.core.publisher.Flux;
+import reactor.core.publisher.Mono;
 
 public interface ProductRepository extends ReactiveCrudRepository<Product, Integer> {
 
-    Flux<Product> findAllByBarcode(String barcode);
+    Mono<Product> findByBarcode(String barcode);
 }
